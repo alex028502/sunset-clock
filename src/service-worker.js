8b38e7e9d00e741c9dc50756df4bd4fe3abe5c6d
alex <alex@example.com>
2017-11-27 14:57:10 +0100

    new service worker scheme
    cache on initial load and then don't reload files again unless service
    worker changes

---
+++ src/service-worker.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const CACHE_PREFIX = 'sunset-clock-';
+const CACHE_NAME = CACHE_PREFIX + 'BUILD_HASH';
+const CACHED_URLS = [
+  'index.html',
+  'app.js',
+  'sw-registration.js',
+  // 'app.js.map', // seems that service worker caching doesn't work for source maps
+  'manifest.json',
+  'favicon.ico',
+  'face.svg',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.open(CACHE_NAME).then(function(cache) {
+    // this page should not request anything that isn't
+    // cached - and everything is cached when the service worker
+    // is installed
+    return cache.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      // if there is no match in the cache, just assume it is the project root
+      return caches.match('index.html');
+    });
+  }));
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return CACHE_NAME !== cacheName && cacheName.startsWith(CACHE_PREFIX);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
+const StringReplacePlugin = require("string-replace-webpack-plugin");
 const path = require('path');
+const uuid = require('uuid');
 
 module.exports = {
-  entry: './src/app.js',
+  plugins: [
+    new StringReplacePlugin(),
+  ],
+  entry: {
+    'app': ['./src/app.js'],
+    'service-worker': ['./src/service-worker.js'],
+    'sw-registration': ['./src/sw-registration.js'],
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    filename: '[name].js',
     publicPath: '/',
   },
 
   module: {
     rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /service-worker.js$/, loader: StringReplacePlugin.replace({
+        replacements: [{
+          pattern: 'BUILD_HASH',
+          replacement: function() {
+            // TODO: figure out how to use the webpack build hash instead of a random string
+            return uuid();
+          },
+        }],
+      })},
     ],
   },
 };
