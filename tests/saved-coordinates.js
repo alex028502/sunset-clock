6f24b4c497a1603c4d421bef0f0fb2719b02751e
alex <alex@example.com>
2017-11-28 13:10:18 +0100

    change test for saved coordinates
    we no longer save error message and timestamp

--- tests/saved-coordinates.js
+++ tests/saved-coordinates.js
@@ -4,9 +4,6 @@ const expect = require('chai').expect;
 
 require('./helpers/integration-setup');
 
-const INITIAL_ERROR_MESSAGE = 'initial error message from local storage';
-const YESTERDAY = new Date((new Date()).getTime() - 2 * 12 * 60 * 60 * 1000);
-
 const EXPECTED_COORDINATE_STRING = '44°0′0″N 55°0′0″E';
 const LATITUDE = 44;
 const LONGITUDE = 55;
@@ -17,8 +14,6 @@ expect(EXPECTED_COORDINATE_STRING).to.include(LONGITUDE);
 global.testVars.storedCoordinates = JSON.stringify({
   latitude: LATITUDE,
   longitude: LONGITUDE,
-  error: INITIAL_ERROR_MESSAGE,
-  timestamp: YESTERDAY,
 });
 
 require('../src/app');
@@ -30,6 +25,3 @@ expect(global.testVars.storedCoordinates).to.be.ok;
 
 expect(dom.serialize(document)).to.include('face.svg');
 expect(dom.serialize(document)).to.include(EXPECTED_COORDINATE_STRING);
-expect(dom.serialize(document)).to.include(INITIAL_ERROR_MESSAGE);
-expect(dom.serialize(document)).to.include('yesterday');
-expect(dom.serialize(document)).not.to.include('Greenwich');
