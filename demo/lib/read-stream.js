465c19a1f3bc864383065c6b25273d7b89be4f8f
alex <alex@example.com>
2017-12-07 21:12:07 +0100

    use library to convert stream to string

--- demo/lib/read-stream.js
+++ demo/lib/read-stream.js
@@ -1,19 +1,10 @@
 'use strict';
 
+const toString = require('stream-to-string');
+
 module.exports = readStream;
 
 function readStream(stream) {
-  // TODO: is this really the easiest way to read a stream?
-  return new Promise(function(resolve) {
-    let string = '';
-    // thanks https://stackoverflow.com/a/35530615/5203563
-    stream.on('data', function(chunk) {
-      string += chunk;
-    });
-
-    // Send the buffer or you can put it into a var
-    stream.on('end', function() {
-      resolve(string);
-    });
-  });
+  // TODO: inline
+  return toString(stream);
 }
