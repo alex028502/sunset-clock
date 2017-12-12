d4a1fee88340d4277750e6748bf7259bfd45d9fc
alex <alex@example.com>
2017-12-12 09:05:23 +0100

    separate service worker transpiling
    so that it can be based on the other compilation results

---
+++ sw.webpack.conf.js
@@ -0,0 +1,35 @@
+const StringReplacePlugin = require('string-replace-webpack-plugin');
+const BannerPlugin = require('webpack').BannerPlugin;
+const path = require('path');
+const uuid = require('uuid');
+
+const BUILD_HASH = uuid(); // not really a hash but could be one day
+
+module.exports = {
+  plugins: [
+    new StringReplacePlugin(),
+    new BannerPlugin(`service worker version ${BUILD_HASH} not [hash]`),
+  ],
+  entry: {
+    'service-worker': ['./src/service-worker.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /service-worker.js$/, loader: StringReplacePlugin.replace({
+        replacements: [{
+          pattern: 'BUILD_HASH',
+          replacement: function() {
+            // TODO: figure out how to use the webpack build hash instead of a random string
+            return uuid();
+          },
+        }],
+      })},
+    ],
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,8 @@
-const StringReplacePlugin = require('string-replace-webpack-plugin');
-const BannerPlugin = require('webpack').BannerPlugin;
 const path = require('path');
-const uuid = require('uuid');
-
-const BUILD_HASH = uuid(); // not really a hash but could be one day
 
 module.exports = {
-  plugins: [
-    new StringReplacePlugin(),
-    new BannerPlugin(`service worker version ${BUILD_HASH} not [hash]`),
-  ],
   entry: {
     'root': ['./src/root.js'],
-    'service-worker': ['./src/service-worker.js'],
     'sw-registration': ['./src/sw-registration.js'],
   },
   output: {
@@ -23,15 +13,6 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /service-worker.js$/, loader: StringReplacePlugin.replace({
-        replacements: [{
-          pattern: 'BUILD_HASH',
-          replacement: function() {
-            // TODO: figure out how to use the webpack build hash instead of a random string
-            return BUILD_HASH;
-          },
-        }],
-      })},
     ],
   },
 };
